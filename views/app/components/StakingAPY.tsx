b8466a7f594a2b4c659b04c3ea5bfe4b83f54f16
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-07-01 10:00:35 -0300

    chore: change staking apy copy

--- views/app/components/StakingAPY.tsx
+++ views/app/components/StakingAPY.tsx
@@ -8,9 +8,11 @@ const StakingAPY = () => {
 
   return (
     <Card sx={{ flexGrow: 1, position: 'relative' }}>
-      <Text>Staking APY</Text>
+      <Text>Staking Yield</Text>
       <Box sx={{ position: 'absolute', bottom: 16 }}>
-        <Text variant="hero">{formatCurrency(apy, 1)}%</Text>
+        <Text variant="hero">
+          {formatCurrency(apy, 1)}% <Text sx={{ fontSize: 4 }}> APY</Text>
+        </Text>
         {/* <Text variant="accent">320%</Text> <Text variant="muted">30d avg</Text> */}
       </Box>
     </Card>
