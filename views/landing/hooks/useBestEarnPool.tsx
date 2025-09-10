ddf8869ac8e1b04c9f181bd28429b3600b0430a7
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-09-10 17:03:18 -0300

    chore: fix llama pools

--- views/landing/hooks/useBestEarnPool.tsx
+++ views/landing/hooks/useBestEarnPool.tsx
@@ -1,44 +1,50 @@
 import { useMemo } from 'react'
-import { useCMSQuery, useMultiFetch } from '../utils'
+import { useMultiFetch } from '../utils'
 
-const earnPoolsQuery = `
-  query {
-    earnPoolsCollection(where: { description_contains: "dgnETH" }) {
-      items {
-        llamaId
-        url
-      }
-    }
-  }
-`
+const llamaPools = [
+  {
+    llamaId: 'e3189532-eced-4e95-8c38-4a9fa04e4879',
+    url: 'https://curve.finance/#/ethereum/pools/factory-stable-ng-224/deposit',
+  },
+  {
+    llamaId: '8f9f8a30-1841-4578-aac2-e3174168588d',
+    url: 'https://yearn.fi/vaults/1/0x961Ad224fedDFa468c81acB3A9Cc2cC4731809f4',
+  },
+  {
+    llamaId: 'd3f24e3d-03ab-454a-ab6c-a353590d1f84',
+    url: 'https://curve.convexfinance.com/stake/ethereum/387',
+  },
+  {
+    llamaId: 'cc314655-9e61-4caf-968f-779a180c7955',
+    url: 'https://www.stakedao.org/strategy?protocol=curve&vault=1-0x3F5882a0cd3F2f2436f0e46D13f6ab7286f8ad0e',
+  },
+]
 
 const DEFILLAMA_POOL_URL = 'https://yields.llama.fi/poolsEnriched?pool='
 
 const useBestEarnPool = () => {
-  const { data: cmsPools } = useCMSQuery(earnPoolsQuery)
   const { data: pools } = useMultiFetch(
-    cmsPools?.data?.earnPoolsCollection?.items.map(
+    llamaPools.map(
       (item: { llamaId: string; url: string }) =>
         `${DEFILLAMA_POOL_URL}${item.llamaId}`
     )
   )
 
   return useMemo(() => {
-    const _cmsPools = cmsPools?.data?.earnPoolsCollection?.items || []
     const _pools = pools?.map((p: any) => p?.data?.[0])
 
-    if (!_cmsPools || !_pools) return null
+    if (!_pools) return null
 
     return _pools
       .filter((pool: any) =>
-        _cmsPools.map((p: any) => p.llamaId).includes(pool?.pool)
+        llamaPools.map((p: any) => p.llamaId).includes(pool?.pool)
       )
       .map((pool: any) => ({
         ...pool,
-        url: _cmsPools.find((p: any) => p.llamaId === pool?.pool)?.url,
+        url: llamaPools.find((p: any) => p.llamaId === pool?.pool)?.url,
       }))
       .sort((a: any, b: any) => b.apy - a.apy)[0]
-  }, [cmsPools, pools])
+  }, [pools])
 }
 
 export default useBestEarnPool
